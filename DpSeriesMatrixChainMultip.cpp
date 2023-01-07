4269d0fc3a9fc82b3eaa1a37db149c37dd7038c9
ayush9009 <97290036+ayush9009@users.noreply.github.com>
2023-01-07 16:21:52 +0530

    Create DpSeriesMatrixChainMultip.cpp

---
+++ DpSeriesMatrixChainMultip.cpp
@@ -0,0 +1,20 @@
+int maxIncreasingDumbbellsSum(vector<int> &nums, int n)
+{
+	// Write your code here
+    int dp[n];
+    for(int i=0;i<n;i++){
+        dp[i]=nums[i];
+    }
+    for(int i=1;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(nums[i]>nums[j] && dp[i]<dp[j]+nums[i]){
+                dp[i]=dp[j]+nums[i];
+            }
+        }
+    }
+    int mx=0;
+    for(auto it:dp){
+        mx=max(it,mx);
+    }
+    return mx;
+}
