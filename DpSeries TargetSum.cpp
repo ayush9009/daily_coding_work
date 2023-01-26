701d75ae2be7b2ccad7037d1b6df1d1d776b9a37
ayush9009 <97290036+ayush9009@users.noreply.github.com>
2023-01-26 09:52:27 +0530

    Create DpSeries TargetSum.cpp

---
+++ DpSeries TargetSum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+         int n=nums.size();
+        target=abs(target);
+        int tSum=accumulate(nums.begin(),nums.end(),0);
+        // if(target==tSum) return pow(2,n-1);
+        if((tSum<target) || (target+tSum)%2!=0) return 0;
+        int s1sum=(target+tSum)/2;
+        
+        
+        // vector<vector<int>> dp(n+1,vector<int>(s1sum+1,0));
+        int dp[n+1][s1sum+1];
+        
+        
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<s1sum+1;j++)
+            {
+                if(i==0) dp[i][j]=0;
+                if(j==0) dp[i][j]=1;
+            }
+        }
+        
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=0;j<s1sum+1;j++)
+            {
+                if(nums[i-1]<=j)
+                {
+                    dp[i][j]=dp[i-1][j-nums[i-1]]+dp[i-1][j];
+                }
+                else dp[i][j]=dp[i-1][j];
+            }
+        }
+        
+        return dp[n][s1sum];
+    }
+};
