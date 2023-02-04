074e13cb86fa54dacc1aebc022aaf871560aa9b5
ayush9009 <97290036+ayush9009@users.noreply.github.com>
2023-02-04 22:41:42 +0530

    Create TreeSeries PreorderTraversal.cpp

---
+++ TreeSeries PreorderTraversal.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int>result;
+    void helper(TreeNode* root){
+        if(root!=NULL){
+            result.push_back(root->val);
+            helper(root->left);
+            helper(root->right);
+        }
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        return result;
+    }
+};
