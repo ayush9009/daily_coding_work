3e037664089bc6a42a03a8f9f1658afc73c18e84
ayush9009 <97290036+ayush9009@users.noreply.github.com>
2023-03-07 10:02:16 +0530

    Create BSTSeries KthLargestElement.cpp

---
+++ BSTSeries KthLargestElement.cpp
@@ -0,0 +1,16 @@
+  Node* helper(Node* root,int &k){
+        if(root==NULL)return NULL;
+        
+        Node* tmp=helper(root->right,k);
+        if(tmp!=NULL)return tmp;
+        k--;
+        if(k==0)return root;
+        return helper(root->left,k);
+    }
+    int kthLargest(Node *root, int k)
+    {
+        //Your code here
+        Node* ans=helper(root,k);
+        if(ans!=NULL)return ans->data;
+        return -1;
+    }
