b9fc628c91e29c582c2d0ae65653b063f3181ebd
ayush9009 <97290036+ayush9009@users.noreply.github.com>
2023-03-13 10:43:28 +0530

    Create BSTSeries CeilFromBST.cpp

---
+++ BSTSeries CeilFromBST.cpp
@@ -0,0 +1,26 @@
+int findCeil(BinaryTreeNode<int> *node, int x){
+    // Write your code here.
+    vector<int>result;
+    BinaryTreeNode<int>*root=node;
+    queue<BinaryTreeNode<int>*>q;
+    q.push(root);
+    while(q.empty()==false){
+        BinaryTreeNode<int>*cur=q.front();
+        q.pop();
+        result.push_back(cur->data);
+        if(cur->left)q.push(cur->left);
+        if(cur->right)q.push(cur->right);
+    }
+    sort(result.begin(),result.end());
+    int mx=x;
+    int flag=0;
+    for(auto it:result){
+        if(it==mx || it>mx){
+            flag=1;
+            mx=it;
+            break;
+        }
+    }
+    if(flag)return mx;
+    return -1;
+}
