b4b235c082fb5f7f17a0fb59006ac27a766b320e
ayush9009 <97290036+ayush9009@users.noreply.github.com>
2023-03-27 17:33:02 +0530

    Create BST Series PreorderTraversalAndBST.cpp
    Given an array arr[ ] of size N consisting of distinct integers, write a program that returns 1 if given array can represent preorder traversal of a possible BST, else returns 0.
    
    Example 1:
    
    Input:
    N = 3
    arr = {2, 4, 3}
    Output: 1
    Explaination: Given arr[] can represent
    preorder traversal of following BST:
                   2
                    \
                     4
                    /
                   3
    Example 2:
    
    Input:
    N = 3
    Arr = {2, 4, 1}
    Output: 0
    Explaination: Given arr[] cannot represent
    preorder traversal of a BST.
    Your Task:
    You don't need to read input or print anything. Your task is to complete the function canRepresentBST() which takes the array arr[] and its size N as input parameters and returns 1 if given array can represent preorder traversal of a BST, else returns 0.
    
    Expected Time Complexity: O(N)
    Expected Auxiliary Space: O(N)
    
    Constraints:
    1 ≤ N ≤ 105
    0 ≤ arr[i] ≤ 105

---
+++ BST Series PreorderTraversalAndBST.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int canRepresentBST(int arr[], int n) {
+        
+        // jaise 40 30 35 80 100
+        // abhi stacak mai 40 30 chle now s.top () i.e 30<35 to root=30 and s.pop() ab agr 30 se chota
+        // koi aya array mai to reutrn false hojaga is sey greater to asake is ey chota ni
+    //   find the next greater ,if u find the next smaller then return false;
+       stack<int>s;
+       int root=INT_MIN;
+       for(int i=0;i<n;i++){
+           if(arr[i]<root)return false;
+           while(s.empty()==false && s.top()<arr[i]){
+               root=s.top();  //stack ki madath se greater element milta jara
+               s.pop();//aur root mai miinimuma assign hojara,agr arr[i]<root to fasle
+            //   jasie root=2 but arr[i]=1 1<2 to reutrn false but 3!<2 return true
+            // sare khail next greater aur smaller ka
+           }
+           s.push(arr[i]);
+       }
+      
+       return true;
+    }
+};
