b143ae5bebadf9ab08730331ba9c5e3ad6f22b76
ayush9009 <97290036+ayush9009@users.noreply.github.com>
2023-04-03 10:18:02 +0530

    TC - O(logN*N) SC-NlogN
    Farmer John has built a new long barn, with N (2 <= N <= 100,000) stalls. The stalls are located along a straight line at positions x1 ... xN (0 <= xi <= 1,000,000,000).
    
    His C (2 <= C <= N) cows don't like this barn layout and become aggressive towards each other once put into a stall. To prevent the cows from hurting each other, FJ wants to assign the cows to the stalls, such that the minimum distance between any two of them is as large as possible. What is the largest minimum distance?
    Input
    t – the number of test cases, then t test cases follows.
    * Line 1: Two space-separated integers: N and C
    * Lines 2..N+1: Line i+1 contains an integer stall location, xi
    Output
    For each test case output one integer: the largest minimum distance.
    Example
    Input:
    
    1
    5 3
    1
    2
    8
    4
    9
    Output:
    
    3
    Output details:
    
    FJ can put his 3 cows in the stalls at positions 1, 4 and 8,
    resulting in a minimum distance of 3.

---
+++ BinarySearch AggressiveCows.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool canPlaceCows(int dist, int arr[], int n, int cows)
+{
+    int co_ord = arr[0], cnt = 1; // yani first cow plce hogyi
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] - co_ord >= dist)
+        {
+            cnt++;
+            co_ord = arr[i]; // yani agli cow place ho chuki
+        }
+        if (cnt == cows)
+            return true; // yani saricows place hogyi
+    }
+    return false; // means u r not able to placed cows
+}
+int largestMinimumDistance(int arr[], int n, int cows)
+{
+    int res;
+    int low = 1, high = arr[n - 1] - arr[0];
+    while (low <= high)
+    {
+        int mid = (low + high) >> 1;
+        if (canPlaceCows(mid, arr, n, cows))
+        {
+            res = mid;
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return res;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int cows;
+        cin >> cows;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        cout << largestMinimumDistance(arr, n, cows);
+    }
+    return 0;
+}
