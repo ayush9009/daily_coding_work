0d8e6ffc2c1e6052bb94cf6408bb205c77dd0ee2
ayush9009 <97290036+ayush9009@users.noreply.github.com>
2023-04-07 12:11:10 +0530

    TC - O(nlogn) SC- O(n)
    There is one meeting room in a firm. There are N meetings in the form of (start[i], end[i]) where start[i] is start time of meeting i and end[i] is finish time of meeting i.
    What is the maximum number of meetings that can be accommodated in the meeting room when only one meeting can be held in the meeting room at a particular time?
    
    Note: Start time of one chosen meeting can't be equal to the end time of the other chosen meeting.
    
    
    Example 1:
    
    Input:
    N = 6
    start[] = {1,3,0,5,8,5}
    end[] =  {2,4,6,7,9,9}
    Output: 
    4
    Explanation:
    Maximum four meetings can be held with
    given start and end timings.
    The meetings are - (1, 2),(3, 4), (5,7) and (8,9)
    Example 2:
    
    Input:
    N = 3
    start[] = {10, 12, 20}
    end[] = {20, 25, 30}
    Output: 
    1
    Explanation:
    Only one meetings can be held
    with given start and end timings.
    
    Your Task :
    You don't need to read inputs or print anything. Complete the function maxMeetings() that takes two arrays start[] and end[] along with their size N as input parameters and returns the maximum number of meetings that can be held in the meeting room.
    
    
    Expected Time Complexity : O(N*LogN)
    Expected Auxilliary Space : O(N)

---
+++ GreedySeries NMeetingsInARoom.cpp
@@ -0,0 +1,41 @@
+struct meeting{
+        int start;
+        int end;
+        int pos;
+};
+class Solution
+{
+    public:
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    bool static comparator(struct meeting m1,meeting m2){
+        if(m1.end < m2.end)return true;
+        else if(m1.end > m2.end)return false;
+        else if(m1.pos < m2.pos)return true;
+        
+        return false;
+    }
+    int maxMeetings(int start[], int end[], int n)
+    {
+        // Your code here
+        struct meeting meet[n];
+        for(int i=0;i<n;i++){
+            meet[i].start=start[i];
+            meet[i].end=end[i];
+            meet[i].pos=i+1;
+        }
+        sort(meet,meet+n,comparator);
+        
+        vector<int>ans;
+        int limit=meet[0].end;
+        ans.push_back(meet[0].end);
+        for(int i=1;i<n;i++){
+            if(meet[i].start > limit){
+            limit=meet[i].end;
+            ans.push_back(meet[i].pos);
+            }
+        }
+        return ans.size();
+        
+    }
+};
