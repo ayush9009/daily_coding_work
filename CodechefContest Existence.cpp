e28a92197a15c508c7d806ecf239bbdbef434997
Ayush Sharma <97290036+ayush9009@users.noreply.github.com>
2023-04-26 21:05:43 +0530

    Create CodechefContest Existence.cpp

---
+++ CodechefContest Existence.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void hlo(){
+    return;
+}
+void singh(){
+    return;
+}
+int main() {
+    
+	// your code goes here
+	int t;
+	singh();
+	cin>>t;
+	while(t--){
+	    singh();
+	    long long x,y;
+	    singh();
+	    cin>>x>>y;
+	    singh();
+	    long long res=x*x*x*x;
+	    singh();
+	    long long res2=y*y;
+	    singh();
+	    long long sm=x*x;
+	    singh();
+	    if(res+4*res2 == 4*sm*y)cout<<"YES"<<endl;
+	    
+	    else cout<<"NO"<<endl;
+	    singh();
+	}
+	return 0;
+}
