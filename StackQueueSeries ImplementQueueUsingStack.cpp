75ee3841f3c46f6725a5b0025d5c8d1ed7266d8c
Ayush Sharma <97290036+ayush9009@users.noreply.github.com>
2023-05-18 09:19:33 +0530

    Update StackQueueSeries ImplementQueueUsingStack.cpp
    TC:O(1) amortised ,SC:O(2N)

--- StackQueueSeries ImplementQueueUsingStack.cpp
+++ StackQueueSeries ImplementQueueUsingStack.cpp
@@ -1,27 +1,86 @@
+class MyQueue {
+public:
+    // stack<int>s1,s2;
+    // MyQueue() {
+        
+    // }
+    
+    // void push(int x) {
+    //     while(!empty()) {
+    //         s2.push(pop());
+    //     }
+        
+    //     s1.push(x);
+        
+    //     while(!s2.empty()) {
+    //         s1.push(s2.top());
+    //         s2.pop();
+    //     }
+    // }
+    
+	// // We don't get any invalid operations to perform for pop and peek.
+	// // Hence I didn't put empty check for pop and peek.
+    // int pop() {
+    //     int x = peek();
+    //     s1.pop();
+    //     return x;
+    // }
+    
+    // int peek() {
+    //     int x = s1.top();
+    //     return x;    
+    // }
+    
+    // bool empty() {
+    //     return s1.empty();
+    // }
 
-    stack<int>s1,s2;
+
+
+
+
+
+
+
+
+
+
+
+    stack<int>input,output;
     MyQueue() {
         
     }
     void push(int x) {
-        s1.push(x);
+       while(!input.empty()){
+           output.push(input.top());
+           input.pop();
+       }
+       input.push(x);
+       while(!output.empty()){
+           input.push(output.top());
+           output.pop();
+       }
        
     }
     int pop() {
-        int  ans=peek();
-        s2.pop();
-        return ans;
+       int x=input.top();
+       input.pop();
+       return x;
+       
     }
     int peek() {
-        if(s2.empty()){
-            while(s1.empty()==false){
-            s2.push(s1.top());
-            s1.pop();
-            }
-        }
-        return s2.top();
+        return input.top();
+        
     }
     bool empty() {
-        return s2.empty();
+      return input.empty();
     }
 };
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
