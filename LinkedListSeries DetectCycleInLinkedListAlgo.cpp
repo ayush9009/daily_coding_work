e09b984ec4222188ba59d9bac9cf17df8e8aa79f
Ayush Sharma <97290036+ayush9009@users.noreply.github.com>
2023-07-08 14:24:42 +0530

    Create LinkedListSeries DetectCycleInLinkedListAlgo.cpp

---
+++ LinkedListSeries DetectCycleInLinkedListAlgo.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+//APPORACH:Floyd's cycle finding algo or tortoise algo or hare algo
+    bool hasCycle(ListNode *head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)return true;
+        }
+        return false;
+    }
+};
